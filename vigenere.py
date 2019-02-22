c0ff4b6197db03cacff3ca6fb5c27d6e5d8b8419
Kurisu <Divinityforvalor@gmail.com>
2019-02-22 14:51:28 -0500

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -25,10 +25,11 @@ def file_information(file_name):
     try:
         file = open(file_name, "r")
         text = file.read()
-        return re.sub(r'[^A-Za-z[0-9]]', '', text).lower()
+        return re.sub(r'[^A-Za-z[0-9]', '', text).lower()
     except FileNotFoundError:
         print("Could not find file source " + file_name + "\n")
-    return
+        print("Exiting program...please check if the file is in the current directory.")
+        exit(0)
 
 
 # Formula C[i] = (p[i] + k[i]) mod 26
@@ -39,8 +40,6 @@ def lowercase_encryption(plaintext_input, key_input):
     encrypted_text = ""
     len_key = len(key_input)
     for i in range(len(plaintext_input)):
-        if plaintext_input[i] == ' ':
-            break
         p = ord(plaintext_input[i]) - OFFSET_LOWERCASE
         k = ord(key_input[i % len_key]) - OFFSET_LOWERCASE
         c = (p + k) % 26
@@ -48,15 +47,50 @@ def lowercase_encryption(plaintext_input, key_input):
     return encrypted_text
 
 
-# Grab information before starting encryption process
-print("Program assumes files are in the current directory!\n")
-plaintext = file_information(input("Plaintext file name: "))
-key = file_information(input("Key file name: "))
-key_length = len(key)
-plaintext_length = len(plaintext)
-padding = 0
+def main():
+    # Grab information before starting encryption process
+    print("Program assumes files are in the current directory!\n")
+    plaintext = file_information(input("Plaintext file name: "))
+    key = file_information(input("Key file name: "))
+    key_length = len(key)
+    plaintext_length = len(plaintext)
+    modified_string = ""
 
-# Calculate necessary padding if below 512 characters
-if plaintext_length < MAX_FILE_SIZE:
-    padding = key_length - (plaintext_length % key_length)
+    # Calculate necessary padding if below 512 characters
+    if plaintext_length < MAX_FILE_SIZE:
+        padding = key_length - (plaintext_length % key_length)
+        should_pad = padding > 0
+        print("Amount to pad:", padding)
+        c = 1
 
+        # Modify the plaintext so its padded to MAX_FILE_SIZE
+        for i in range(plaintext_length + padding):
+            if not should_pad and i >= plaintext_length:
+                break
+            elif should_pad and i >= plaintext_length:
+                print("padding count:", c)
+                c += 1
+                modified_string = modified_string + "x"
+            else:
+                modified_string = modified_string + plaintext[i]
+    elif plaintext_length > MAX_FILE_SIZE:
+        # Modify the plaintext so its only the MAX_FILE_SIZE
+        for i in range(MAX_FILE_SIZE):
+            modified_string = modified_string + plaintext[i]
+
+    plaintext_length = len(modified_string)
+    print("PL:", plaintext_length)
+    print("KL:", key_length)
+
+    plaintext = modified_string
+    cipher = lowercase_encryption(plaintext, key)
+
+    print("Key:", formatted_print(key))
+    print("\nPlaintext:", formatted_print(plaintext))
+    print("\nCiphertext:", formatted_print(cipher))
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
